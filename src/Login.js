5c2824fba43ca2c4b554094aba27996e478bc11e
daisuke719 <daisuke719@ezweb.ne.jp>
2022-08-15 01:46:21 +0900

    inputタグの修正

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
 import classes from "./style.module.css";
 import { useNavigate } from "react-router-dom"; 
+import { useState } from "react";
 
 export const Login = () => {
 
+  const [adress, setAdress] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const GoTopPage = () => {
@@ -12,11 +15,13 @@ export const Login = () => {
   return (
     <div>
       <h1>筋肉管理委員会</h1>
-      <input value="メールアドレス"></input>
+      <input placeholder="メールアドレス" onChange={(e)=>setAdress(e.target.value)}></input>
       <br></br>
-      <input value="パスワード"></input>
+      <input placeholder="パスワード" onChange={(e)=>setPassword(e.target.value)}></input>
       <br></br>
       <br></br>
+      <p>入力したアドレスは"{adress}"です。</p>
+      <p>入力したパスワードは"{password}"です。</p>
    
         <button type="button" class={classes.loginButton} onClick={() => GoTopPage()} >ログイン</button>
 
