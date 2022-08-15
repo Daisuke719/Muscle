1632b5c5c71a63280d51fe07156aabb0fccadd55
daisuke719 <daisuke719@ezweb.ne.jp>
2022-08-15 12:27:52 +0900

    toppageの概要を作成

--- src/TopPage.js
+++ src/TopPage.js
@@ -1,8 +1,13 @@
 import classes from "./style.module.css";
 import { useNavigate } from "react-router-dom"; 
+import { useState } from "react";
 
 export const TopPage = () => {
-
+    const [date, setDate] = useState("");
+    const [point, setPoint] = useState("");
+    const [train, setTrain] = useState("");
+    const [time, setTime] = useState("");
+    const [happy, setHappy] = useState("");
     const navigate = useNavigate();
 
     const BackToLogin = () => {
@@ -11,7 +16,29 @@ export const TopPage = () => {
 
     return(
         <>
-            <h1>TOPPAGEだお</h1>
+            <h1>トレーニング日程の調整</h1>
+            <button type="button" class={classes.loginButton}>新規登録</button>
+            <br></br>
+            <hr></hr>
+            <div class={classes.backcolor}>
+                <h3>登録したいトレーニングを入力してください</h3>
+                <div>日付　　　　：　<input onChange={(e)=>setDate(e.target.value)}></input></div>
+                <div>部位　　　　：　<input onChange={(e)=>setPoint(e.target.value)}></input></div>
+                <div>内容　　　　：　<input onChange={(e)=>setTrain(e.target.value)}></input></div>
+                <div>時間　　　　：　<input onChange={(e)=>setTime(e.target.value)}></input></div>
+                <div>達成できた？：　<input onChange={(e)=>setHappy(e.target.value)}></input></div>
+            </div>
+            <br></br>
+            <div class={classes.backcolor2}>
+                <h3>前回の筋トレ記録</h3>
+                <div>日付　　　　：　{date}</div>
+                <div>部位　　　　：　{point}</div>
+                <div>内容　　　　：　{train}</div>
+                <div>時間　　　　：　{time}</div>
+                <div>達成できた？：　{happy}</div>
+
+            </div>
+            <br></br>
             <button type="button" class={classes.loginButton} onClick={() => BackToLogin()}>戻る</button>
         </>
     )
